0c3ab53d55d03b097b852d86aaf94099708f7c40
lukeloewe <81586834+lukeloewe@users.noreply.github.com>
2022-11-29 19:13:46 +0100

    bike rent and return functionality

--- webapp/app.py
+++ webapp/app.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
 
 from . import create_app
 from .models import *
@@ -33,13 +33,34 @@ def add():
 def hello():
     return render_template('hello.html', email=current_user.email)
 
-# TODO: rent, return functionality
-@app.route("/bike-<id>", methods=['GET', 'POST'])
-@app.route("/bike-<id>/<operation>", methods=['GET', 'POST'])
+@app.route("/bike<id>", methods=['GET', 'POST'])
+@app.route("/bike<id>/<operation>", methods=['GET', 'POST'])
 @auth_required()
-def bike(id):
+def bike(id, operation=None):
+    ride_db = Ride.query.filter_by(bike_id=id, end_time=None).first()
+    if request.method == 'POST':
+        if request.content_type == 'application/json':
+            ride_req = request.get_json()
+        elif request.content_type == 'application/x-www-form-urlencoded':
+            ride_req = request.form
+        else:
+            return "Unsupported content type"
+        # time format: 2004-10-19 10:23:54
+        if operation == 'rent':
+            add_instance(Ride, user_id=current_user.id, bike_id=id, start_time=ride_req['start_time'])
+            return redirect('/bike' + id)
+        elif operation == 'return':                
+            edit_instance(Ride, ride_db.id, end_time=ride_req['end_time'])
+            return redirect('/bike' + id)
+
+    if ride_db is None:
+        operation = "rent"
+    elif ride_db.user_id == current_user.id:
+        operation = "return"
+    else:
+        operation = "unavailable"
     bike = get_instance(Bike, id)
-    return render_template('bike.html', bike=bike)
+    return render_template('bike.html', bike=bike, operation=operation, button_text=operation.capitalize())
 
 @app.route("/bike-management", methods=['GET', 'POST'])
 @app.route("/bike-management/<operation>", methods=['GET', 'POST'])
